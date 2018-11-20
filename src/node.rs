c716a72e39179aa164ca920d66d33391301babdd
Kévin NAUDIN <naudin.kevin@outlook.fr>
2018-11-21 00:40:05 +0100

    [Node] Made use of smart pointers and md5 hash.

--- src/node.rs
+++ src/node.rs
@@ -1,27 +1,32 @@
+extern crate md5;
+
 use super::data::*;
-use std::io::Write;
-use std::io::stdout;
+use std::cell::RefCell;
+use std::rc::Rc;
+use self::md5::*;
 
 pub struct Node{
     data : Data,
     hash : Option<String>,
     is_head : bool,
     is_leaf : bool,
-    children : Vec<Node>
+    children : Vec<Rc<RefCell<Node>>>
 }
 
 impl Node{
-    pub fn new(node_data : Data) -> Node {
-        Node{
+    pub fn new(node_data : Data) -> Rc<RefCell<Node>> {
+        let node = Rc::new(RefCell::new(Node{
             data : node_data,
             hash : None,
             is_head : false,
             is_leaf : false,
             children : Vec::new()
-        }
+        }));
+        node.borrow_mut().compute_hash();
+        node
     }
 
-    pub fn add_child(&mut self, node : Node) -> bool {
+    pub fn add_child(&mut self, node : Rc<RefCell<Node>>) -> bool {
         let mut added_node = true;
 
         if self.children.len() < 2 as usize {
@@ -37,14 +42,47 @@ impl Node{
         &self.data
     }
 
-    pub fn get_all_data(&self){
+    pub fn print_all_data(&self){
         print!("Data name: '{}' Held information -> ", self.data.get_name());
         match self.data.get_value() {
-            DataType::String(v) => println!("{}", v),
-            DataType::Integer(v)  => println!("{}", v),
-            DataType::Float(v)  => println!("{}", v)
+            DataType::String(v) => print!("{}", v),
+            DataType::Integer(v)  => print!("{}", v),
+            DataType::Float(v)  => print!("{}", v)
         }
-        stdout().flush();
-        &self.children.iter().for_each(|child| child.get_all_data());
+        if let Some(hash) = self.hash.clone(){
+            println!(" hash: {}", hash);
+        }
+        &self.children.iter().for_each(|child| child.borrow_mut().print_all_data());
+    }
+
+    pub fn set_head(&mut self, is_head : bool){
+        self.is_head = is_head;
+    }
+
+    pub fn get_hash(&self) -> String{
+        let mut hash = String::new();
+        if let Some(str) = self.hash.clone(){
+            hash = str;
+        }
+        hash
+    }
+
+    pub fn compute_hash(&mut self){
+        let mut value = String::new();
+        // If we have children, we build our hash according to their hash
+        if self.children.len() > 0{
+            self.children.iter().for_each(|child|
+                value.push_str(&*child.borrow_mut().get_hash())
+            );
+        }
+        // If we don't have children, we build our hash according to our data
+        else{
+            match self.get_data().get_value(){
+                    DataType::String(s) => value.push_str(s),
+                    DataType::Integer(i) => value.push_str(&*i.to_string()),
+                    DataType::Float(f) => value.push_str(&*f.to_string())
+            }
+        }
+        self.hash.get_or_insert(format!("{:x}", md5::compute(value)));
     }
 }
