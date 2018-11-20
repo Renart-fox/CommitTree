90dfce0f6abf02dd0ea12a2f0e0d77e2eedb1dd5
Kévin NAUDIN <naudin.kevin@outlook.fr>
2018-11-20 21:28:46 +0100

    [MerkleTree] Added children test.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use node::*;
 fn main() {
 
     let first_node = Node::new(Data::new(String::from("My age"), DataType::Integer(21)));
+    let twin_node = Node::new(Data::new(String::from("My height"), DataType::Integer(178)));
     let mut second_node = Node::new(Data::new(String::from("A float"), DataType::Float(3.14)));
     let mut third_node = Node::new(Data::new(String::from("This is a"), DataType::String(String::from("Working tree."))));
 
     second_node.add_child(first_node);
+    second_node.add_child(twin_node);
     third_node.add_child(second_node);
 
     third_node.get_all_data();
