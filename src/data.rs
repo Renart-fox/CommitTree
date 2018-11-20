9dcc98cfc14ac2476bdee9e436635fdb977abd44
Kévin NAUDIN <naudin.kevin@outlook.fr>
2018-11-20 21:25:22 +0100

    [Data] Used enum types.

--- src/data.rs
+++ src/data.rs
@@ -1,15 +1,31 @@
 // Defines the data contained in a node
+
+pub enum DataType{
+    String(String),
+    Float(f64),
+    Integer(u32)
+}
+
+// A data is composed of a single value
 pub struct Data<T> {
+    name : String,
     value : T
 }
 
 impl<T> Data<T>{
-    pub fn new(data_value : T) -> Data<T> {
-        let data = Data{value: data_value};
-        data
+    pub fn new(data_name : String, data_value : T) -> Data<T> {
+        Data{name: data_name, value: data_value}
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
     }
 
     pub fn get_value(&self) -> &T {
         &self.value
+    }
+
+    pub fn set_value(&mut self, data_value : T) {
+        self.value = data_value;
     }
 }
