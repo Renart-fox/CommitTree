699e4d31d384191f07a9025f7abe03c79163aa94
Kévin NAUDIN <naudin.kevin@outlook.fr>
2018-11-21 14:04:25 +0100

    [Tree] Trees now support multiple branches.

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,20 @@ use tree::*;
 
 fn main() {
 
-    let first_node = Node::new(Data::new(String::from("My age"), DataType::Integer(21)));
-    let second_node = Node::new(Data::new(String::from("A float"), DataType::Float(3.14)));
-    let third_node = Node::new(Data::new(String::from("This is a"), DataType::String(String::from("Working tree."))));
+    let first_node = Node::new(Data::new(String::from("My age"), DataType::Integer(21)), String::from("Added my age."));
+    let second_node = Node::new(Data::new(String::from("A float"), DataType::Float(3.14)), String::from("Defined PI."));
+    let third_node = Node::new(Data::new(String::from("This is a"), DataType::String(String::from("Working tree."))), String::from("Commented line"));
+    let mut fourth_node = Node::new(Data::new(String::from("Random number"), DataType::Integer(21)), String::from("BRANCH ERROR"));
 
     let mut merkle_tree = Tree::new(first_node);
-    merkle_tree.add(second_node);
-    merkle_tree.add(third_node);
+    merkle_tree.push(second_node, String::from("master"));
+    merkle_tree.push(third_node, String::from("master"));
 
-    merkle_tree.print_all_data();
+    merkle_tree.print_all_data(String::from("master"));
+
+    let error = merkle_tree.push(fourth_node.clone(), String::from("dev")); //error
+    println!("{}", error);
+
+    merkle_tree.new_branch(fourth_node, String::from("dev"));
+    merkle_tree.print_all_data(String::from("dev"));
 }
--- src/tree.rs
+++ src/tree.rs
@@ -2,28 +2,63 @@ use super::data::*;
 use super::node::*;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::collections::HashMap;
 
 pub struct Tree{
-    head : Rc<RefCell<Node>>,
+    branches : HashMap<String, Rc<RefCell<Node>>>,
+    last_working_branch : String,
     size : u32
 }
 
 impl Tree{
     pub fn new(tree_head : Rc<RefCell<Node>>) -> Tree{
         tree_head.borrow_mut().set_head(true);
-        Tree{
-            head : tree_head,
+        let mut tree = Tree{
+            branches : HashMap::new(),
+            last_working_branch : "master".to_string(),
             size : 1
+        };
+        tree.branches.insert("master".to_string(), tree_head);
+        tree
+    }
+
+    pub fn push(&mut self, node : Rc<RefCell<Node>>, branch : String) -> String{
+        if let Some(branch_head) = self.branches.clone().get(&branch){
+            node.borrow_mut().add_child(branch_head.clone());
+            node.borrow_mut().compute_hash();
+            self.branches.insert("master".to_string(), node);
+            self.last_working_branch = branch;
+            self.size += 1;
+            "Commit pushed".to_string()
+        }
+        else {
+            "[error] branch does not exist".to_string()
         }
     }
 
-    pub fn add(&mut self, node : Rc<RefCell<Node>>){
-        node.borrow_mut().add_child(self.head.clone());
-        node.borrow_mut().compute_hash();
-        self.head = node;
+    pub fn new_branch(&mut self, node : Rc<RefCell<Node>>, branch : String) -> String{
+        if self.branches.clone().contains_key(&branch){
+            "[error] branch with the same name already exist".to_string()
+        }
+        else {
+            if let Some(branch_head) = self.branches.clone().get(&self.last_working_branch){
+                node.borrow_mut().add_child(branch_head.clone());
+            }
+            self.branches.insert(branch.clone(), node);
+            self.last_working_branch = branch;
+            self.size += 1;
+            "branch created".to_string()
+        }
     }
 
-    pub fn print_all_data(&self){
-        self.head.borrow_mut().print_all_data();
+    pub fn print_all_data(&self, branch : String) -> String{
+        if let Some(branch_head) = self.branches.get(&branch){
+            println!("Branch {}", branch);
+            branch_head.borrow_mut().print_all_data();
+            "Printed".to_string()
+        }
+        else {
+            "[error] branch does not exist".to_string()
+        }
     }
 }
