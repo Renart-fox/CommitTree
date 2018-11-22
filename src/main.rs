d7df2ac88adac1a5508e7111d511c1a4bbc5a3a0
Kévin NAUDIN <naudin.kevin@outlook.fr>
2018-11-22 21:45:33 +0100

    [Test] Unnecessary mut removed.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let first_node = Node::new(Data::new(String::from("My age"), DataType::Integer(21)), String::from("Added my age."));
     let second_node = Node::new(Data::new(String::from("A float"), DataType::Float(3.14)), String::from("Defined PI."));
     let third_node = Node::new(Data::new(String::from("This is a"), DataType::String(String::from("Working tree."))), String::from("Commented line"));
-    let mut fourth_node = Node::new(Data::new(String::from("Random number"), DataType::Integer(42)), String::from("New branch!"));
+    let fourth_node = Node::new(Data::new(String::from("Random number"), DataType::Integer(42)), String::from("New branch!"));
 
     let mut merkle_tree = Tree::new(first_node);
     merkle_tree.push(second_node, String::from("master"));
